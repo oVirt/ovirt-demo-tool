6c4451f455e81abce545ab26b7471c10add9e3ec
gbenhaim <galbh2@gmail.com>
2017-11-20 18:04:46 +0200

    Configure master branch to build master images
    Configure master branch to work with master-tested repo.
    
    Change-Id: I51cf16f5e6cf534a8fc3ccb5442f051ae57353fd
    Signed-off-by: gbenhaim <galbh2@gmail.com>

--- deploy/control_config.sh
+++ deploy/control_config.sh
@@ -2,13 +2,13 @@
 
 SRV_USERNAME=ovirt-demo-tool
 SRV_HOSTNAME=templates.ovirt.org
-SRV_PATH=/var/www/html/bundles/ovirt-demo-tool/4.2/unstable
+SRV_PATH=/var/www/html/bundles/ovirt-demo-tool/master/unstable
 
 TEMPLATE_REPO_PATH=http://templates.ovirt.org/repo/repo.metadata
 
-SUITE_NAME=basic-suite-4-2
+SUITE_NAME=basic-suite-master
 
-RELEASE_RPM=http://resources.ovirt.org/pub/yum-repo/ovirt-release42-pre.rpm
+RELEASE_RPM=http://resources.ovirt.org/pub/yum-repo/ovirt-release-master.rpm
 
 ENGINE_TEMPLATE=el7.4-base
 HOST_TEMPLATE=el7.4-base
