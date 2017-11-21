2d564bf6fb6fd538e00bb7d096134fde872b6a87
gbenhaim <galbh2@gmail.com>
2017-11-21 18:43:13 +0200

    Adopt the new name of the project
    Change-Id: I693c4d65d9fac0b1c3e222606d85f942efacbe2e
    Signed-off-by: gbenhaim <galbh2@gmail.com>

--- deploy/control_config.sh
+++ deploy/control_config.sh
@@ -6,7 +6,7 @@ SRV_PATH=/var/www/html/bundles/ovirt-demo-tool/4.2/unstable
 
 TEMPLATE_REPO_PATH=http://templates.ovirt.org/repo/repo.metadata
 
-SUITE_NAME=basic-suite-4-2
+SUITE_NAME=ovirt-orb-4-2
 
 RELEASE_RPM=http://resources.ovirt.org/pub/yum-repo/ovirt-release42-pre.rpm
 
